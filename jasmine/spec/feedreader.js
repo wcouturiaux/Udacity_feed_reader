3dbcc5b24ff96ffa32f759d9f5c3d58163b6303a
Will C <w.l.couturiaux@gmail.com>
2018-09-18 22:48:58 -0400

    Convert spaces to tabs, update it('has valid urls')

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -9,81 +9,82 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
-    describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
-        /*
-        *Regular expression from https://gist.github.com/dperini/729294
-        *Author: Diego Perini
-        *Version Date: 2018-09-12
-        */
-        var urlTest = new RegExp(/^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i);
-        var haveURLs = true;
-        var feedURL;
+	/* This is our first test suite - a test suite just contains
+	* a related set of tests. This suite is all about the RSS
+	* feeds definitions, the allFeeds variable in our application.
+	*/
+	describe('RSS Feeds', function() {
+		/* This is our first test - it tests to make sure that the
+		 * allFeeds variable has been defined and that it is not
+		 * empty. Experiment with this before you get started on
+		 * the rest of this project. What happens when you change
+		 * allFeeds in app.js to be an empty array and refresh the
+		 * page?
+		 */
 
-        it('are defined', function() {
-            expect(allFeeds).toBeDefined();
-            expect(allFeeds.length).not.toBe(0);
-        });
+		/*
+		*Regular expression from https://gist.github.com/dperini/729294
+		*Author: Diego Perini
+		*Version Date: 2018-09-12
+		*/
+		var urlTest = new RegExp(/^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z0-9\u00a1-\uffff][a-z0-9\u00a1-\uffff_-]{0,62})?[a-z0-9\u00a1-\uffff]\.)+(?:[a-z\u00a1-\uffff]{2,}\.?))(?::\d{2,5})?(?:[/?#]\S*)?$/i);
+		var validUrls = true;
 
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
-        it('has URL', function() {
-            for(var i=0; i < allFeeds.length; i++){
-                feedURL = allFeeds[i].url;
-                if (!urlTest.test(feedURL)){
-                    haveURLs = false;
-                }
-            }
-            expect(haveURLs).toBe(true);
-        });
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
-    });
+		it('are defined', function() {
+			expect(allFeeds).toBeDefined();
+			expect(allFeeds.length).not.toBe(0);
+		});
 
 
-    /* TODO: Write a new test suite named "The menu" */
+		/* TODO: Write a test that loops through each feed
+		 * in the allFeeds object and ensures it has a URL defined
+		 * and that the URL is not empty.
+		 */
+		it('has valid URL', function() {
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+			for(var i=0; i < allFeeds.length; i++){
+				if(!urlTest.test(allFeeds[i].url)){
+					validUrls = false;
+				}
+				expect(allFeeds[i].url).toBeDefined();
+				expect(validUrls).toBe(true);
+			}
+		});
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+		/* TODO: Write a test that loops through each feed
+		 * in the allFeeds object and ensures it has a name defined
+		 * and that the name is not empty.
+		 */
+	});
 
-    /* TODO: Write a new test suite named "Initial Entries" */
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+	/* TODO: Write a new test suite named "The menu" */
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+		/* TODO: Write a test that ensures the menu element is
+		 * hidden by default. You'll have to analyze the HTML and
+		 * the CSS to determine how we're performing the
+		 * hiding/showing of the menu element.
+		 */
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+		 /* TODO: Write a test that ensures the menu changes
+		  * visibility when the menu icon is clicked. This test
+		  * should have two expectations: does the menu display when
+		  * clicked and does it hide when clicked again.
+		  */
+
+	/* TODO: Write a new test suite named "Initial Entries" */
+
+		/* TODO: Write a test that ensures when the loadFeed
+		 * function is called and completes its work, there is at least
+		 * a single .entry element within the .feed container.
+		 * Remember, loadFeed() is asynchronous so this test will require
+		 * the use of Jasmine's beforeEach and asynchronous done() function.
+		 */
+
+	/* TODO: Write a new test suite named "New Feed Selection" */
+
+		/* TODO: Write a test that ensures when a new feed is loaded
+		 * by the loadFeed function that the content actually changes.
+		 * Remember, loadFeed() is asynchronous.
+		 */
 }());
